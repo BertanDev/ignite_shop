222d4a95275d20e0413b28293e387ecd0017fb87
João Vitor Bertan <72395637+BertanDev@users.noreply.github.com>
2022-11-27 15:38:28 -0300

    code: finished

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,17 @@
+import { styled } from "..";
+
+export const Container = styled('div', {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    minHeight: "100vh",
+    padding: '0 2rem',
+})
+
+export const Header = styled('header', {
+    padding: '1rem 0',
+    width: '100%',
+    maxWidth: 1180,
+    margin: '0 auto',
+})
